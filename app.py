a448c670c0d165e26148438f3e0dc1aed2f4f6e4
Carina <carrie.poell@gmail.com>
2019-07-25 19:13:53 +0200

    wire up index.html to the Flask app.

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'tandev'
+app.config['MONGO_URI']=os.environ.get("MONGO_URI")
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+def get_index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+    app.run(host=os.getenv('IP'), port=(os.getenv('PORT')), debug="True")
