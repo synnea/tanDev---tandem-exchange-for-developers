0fe4687ae6e0a6b66327769feefd32e77cad4868
Carina <carrie.poell@gmail.com>
2019-08-02 12:55:51 +0200

    add common.js

--- static/js/common.js
+++ static/js/common.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
-
-    
+    $(function () {
+        $("#hamburger").on("click", function () {
+            $("p").addClass(".test-class");
+        });
     });
+
+});
