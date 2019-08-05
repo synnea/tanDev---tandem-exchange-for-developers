5fc38cb48a1df4601fa5311944ac3b07ba69745e
Carina <carrie.poell@gmail.com>
2019-08-05 22:43:48 +0200

    wire up pymongo app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 from flask import Flask, render_template, url_for, redirect, request
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 from bson.objectid import ObjectId 
 
 app = Flask(__name__)
@@ -9,11 +10,19 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'tandev'
 app.config['MONGO_URI']=os.environ.get("MONGO_URI")
 
+mongo = PyMongo(app)
 
-@app.route('/')
-def get_index():
-    return render_template("pages/index.html", active="home")
+client = MongoClient(app.config['MONGO_URI'])
+db = client.tandev
 
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def home_page():
+
+
+    carousel = db.profile.aggregate( [ { "$sample": { "size": 6 } } ])
+    carousel = list(carousel)
+    return render_template("pages/index.html", active="home", carousel=carousel)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=(os.getenv('PORT')), debug="True")
