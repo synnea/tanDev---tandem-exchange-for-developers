36bca98bd1e17494a5bed74a047ba2f14ddf8876
Carina <carrie.poell@gmail.com>
2019-08-08 19:45:08 +0200

    add prev and next btns

--- static/js/index.js
+++ static/js/index.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
     $('#index-carousel .carousel-item:first').addClass('active');
 });
 
+
+$(function () {
+    $(".").on("click", function () {
+        $(".main").hide();
+        $(".dashboard-container").fadeIn(300);
+       $(".content-container").removeClass("background");
+    });
+});
