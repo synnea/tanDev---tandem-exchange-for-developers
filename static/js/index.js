9fefd857d9271051cbab36afdddbddfa05cb063a
Carina <carrie.poell@gmail.com>
2019-08-08 20:07:34 +0200

    remove controls

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,5 @@
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     $('#index-carousel .carousel-item:first').addClass('active');
 });
 
 
-$(function () {
-    $(".").on("click", function () {
-        $(".main").hide();
-        $(".dashboard-container").fadeIn(300);
-       $(".content-container").removeClass("background");
-    });
-});
