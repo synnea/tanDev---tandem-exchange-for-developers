311f0831f8a99ae468e09e0689cc9140b7c5a62c
Carina <carrie.poell@gmail.com>
2019-08-09 11:28:40 +0200

    make indicators visible

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,6 @@
+// Add the class of active to the first carousel item upon load. This solution was also taken from Anna Greaves' FamilyHub project.
+
+
 document.addEventListener("DOMContentLoaded", function () {
     $('#index-carousel .carousel-item:first').addClass('active');
 });
