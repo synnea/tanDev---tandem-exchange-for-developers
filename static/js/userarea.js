81124aae43b7ab7f64225f34e2d941ed874a3134
Carina <carrie.poell@gmail.com>
2019-08-22 17:18:23 +0200

    get jQuery to work.

--- static/js/userarea.js
+++ static/js/userarea.js
@@ -1,4 +1,16 @@
-
 $(document).ready(function () {
 
+    $(".skill-options").hide();
+
+    $(function() {
+    $("#python-box").on("click", function () {
+        $("#python-options").slideToggle();
+    });
+});
+
+$(function() {
+    $("#sql-box").on("click", function () {
+        $("#sql-options").slideToggle();
+    });
+});
 });
