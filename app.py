f74a18c9f1ca0b8faf15f87bf780466d93d460bb
Carina <carrie.poell@gmail.com>
2019-09-03 23:59:05 +0200

    Debugging: heroku

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ def register():
             "published": "",
             "display": False
         }
-        db.profile.insert_one(register)
+        db.profile.insert_one({register}, { writeConcern: { w: "1" }})
         session['username'] = request.form.get('username')
         return redirect(url_for('newprofile', username = session['username']))
 
