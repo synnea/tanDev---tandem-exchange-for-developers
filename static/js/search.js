2903b212b1977f0b786f5bcc8e9c110d540c8d02
Carina <carrie.poell@gmail.com>
2019-09-15 20:20:03 +0200

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -1,49 +1,46 @@
 $(document).ready(function () {
 
-    // Activate sidebar toggle functionality
+            // Activate sidebar toggle functionality
 
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active', 500);
-    });
+            if ($(window).width() <= 800) {
+                $("#sidebar").removeClass("active");
+                $('#content').removeClass('content-visibility');
 
-    if ($(window).width() < 800) {}
-
-
-    // Activate the custom multiple select dropdown picker.
-
-    $(function () {
-        $('select').selectpicker();
-    });
+                $('#sidebarCollapse').on('click', function () {
+                    $('#sidebar').toggleClass('active', 500);
+                    $('#content').toggleClass('content-visibility');
+                });
+            };
 
 
-    // Clear the selected items in the dropdown menus on the search page.
+            if ($(window).width() > 800) {
+                $("#sidebar").addClass("active");
+                $('#content').removeClass('content-visibility');
 
-    $(function () {
-
-
-
-        $('#districtRefresh').on("click", function () {
-            var itemSelectorOption = $('#districtSelect.selectpicker option:selected');
-            itemSelectorOption.remove();
-            $('#districtSelect').selectpicker('refresh');
-        })
-
-    });
-
-    if ($(window).width() <= 800) {
-        $(".search-card").addClass("d-none");
-    }
-
-    // Call the same functions upon window resize.
-
-    $(window).on('resize', function () {
-
-        if ($(window).width() <= 800) {
-
-            $(".search-card").addClass("d-none");
-        }
-    });
+            }
 
 
-});
+            $(window).on('resize', function() {
+
+                if ($(window).width() <= 800) {
+                    $("#sidebar").removeClass("active");
+                    $('#content').removeClass('content-visibility');
+    
+                    $('#sidebarCollapse').on('click', function () {
+                        $('#sidebar').toggleClass('active', 500);
+                        $('#content').toggleClass('content-visibility');
+                    });
+                };
+
+                if ($(window).width() > 800) {
+                    $("#sidebar").addClass("active");
+                    $('#content').removeClass('content-visibility');
+    
+                }
+            });
+
+        });
+
+
+
