3eb61bfeff069c8085232a4bd94fa4c7f6744ffd
Carina <carrie.poell@gmail.com>
2019-09-15 18:42:50 +0200

    finish JavaScript for search.html.

--- static/js/search.js
+++ static/js/search.js
@@ -1,33 +1,46 @@
 $(document).ready(function () {
 
-    // Activate sidebar toggle functionality
+            // Activate sidebar toggle functionality
 
+
+            if ($(window).width() <= 800) {
+                $("#sidebar").removeClass("active");
+                $('#content').removeClass('content-visibility');
+
+                $('#sidebarCollapse').on('click', function () {
+                    $('#sidebar').toggleClass('active', 500);
+                    $('#content').toggleClass('content-visibility');
+                });
+            };
+
+
+            if ($(window).width() > 800) {
+                $("#sidebar").addClass("active");
+                $('#content').removeClass('content-visibility');
+
+            }
+
+
+            $(window).on('resize', function () {
+
+                if ($(window).width() <= 800) {
+                    $("#sidebar").removeClass("active");
+                    $('#content').removeClass('content-visibility');
     
-    if ($(window).width() <= 800) {
-        $("#sidebar").addClass("active");
+                    $('#sidebarCollapse').on('click', function () {
+                        $('#sidebar').toggleClass('active', 500);
+                        $('#content').toggleClass('content-visibility');
+                    });
+                };
 
-        $('#sidebarCollapse').on('click', function() {
-        $('#sidebar').toggleClass('active', 500);
-        $('#content').toggleClass('content-visibility');
+                if ($(window).width() > 800) {
+                    $("#sidebar").addClass("active");
+                    $('#content').removeClass('content-visibility');
+    
+                }
+            });
+
         });
-    };
-
-
-    if ($(window).width() > 800) {
-        $("#sidebar").removeClass("active");
-
-    }
-
-
-  $(window).on('resize', function () {
-
-        if ($(window).width() <= 800) {
-            $("#sidebar").removeClass("active");
-            $('#sidebar').toggleClass('active', 500);
-            $('#content').toggleClass('content-visibility');
-        }
-    });
 
 
 
-});
