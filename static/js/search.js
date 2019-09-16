9323f25ba0f3273e38fb9f1691a8869974060b7f
Carina <carrie.poell@gmail.com>
2019-09-16 19:01:07 +0200

    Fix sidebar behavior.

--- static/js/search.js
+++ static/js/search.js
@@ -1,46 +1,13 @@
 $(document).ready(function () {
 
-            // Activate sidebar toggle functionality
+    // Activate sidebar toggle functionality
 
+    if ($(window).width() > 800) {
+        $('sidebar').removeClass('active');
+    }
 
-            if ($(window).width() <= 800) {
-                $("#sidebar").removeClass("active");
-                $('#content').removeClass('content-visibility');
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
 
-                $('#sidebarCollapse').on('click', function () {
-                    $('#sidebar').toggleClass('active', 500);
-                    $('#content').toggleClass('content-visibility');
-                });
-            };
-
-
-            if ($(window).width() > 800) {
-                $("#sidebar").removeClass("active");
-                $('#content').removeClass('content-visibility');
-
-            }
-
-
-            $(window).on('resize', function() {
-
-                if ($(window).width() <= 800) {
-                    $("#sidebar").removeClass("active");
-                    $('#content').removeClass('content-visibility');
-    
-                    $('#sidebarCollapse').on('click', function () {
-                        $('#sidebar').toggleClass('active', 500);
-                        $('#content').toggleClass('content-visibility');
-                    });
-                };
-
-                if ($(window).width() > 800) {
-                    $("#sidebar").addClass("active");
-                    $('#content').removeClass('content-visibility');
-    
-                }
-            });
-
-        });
-
-
-
+});
