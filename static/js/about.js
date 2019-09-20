8adc057cfd2238be1047b95134e185948e11f95c
Carina <carrie.poell@gmail.com>
2019-09-20 19:17:44 +0200

    Fix bugs & tweak user experience.

--- static/js/about.js
+++ static/js/about.js
@@ -1,9 +1,17 @@
-
 $(document).ready(function () {
 
+    // initialize the scroll animations.
 
-$(function() {
+  $(function () {
     AOS.init();
+  });
+
+    // pushes down the overlay to prevent it from overlapping the menu options.
+
+  $(function () {
+    $('#hamburger').on("click", function () {
+      $("#about-overlay").toggleClass("overlay-open")
+    });
   });
 
 });
